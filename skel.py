250fb5c2f95191f4896a92af621bbe43db8bde67
Ivo Tamboer <ivo.tamboer@letshare.com>
2015-08-06 15:29:57 +0200

    http authentication (basic) implemented but not linked

--- skel.py
+++ skel.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from MySQLdb import connect
 from MySQLdb import Error
 from BaseHTTPServer import BaseHTTPRequestHandler
@@ -9,7 +8,9 @@ from oauthlib.oauth2.rfc6749.errors import InvalidClientIdError
 from oauthlib.oauth2.rfc6749.errors import InvalidRequestError
 from oauthlib.oauth2.rfc6749.errors import MissingClientIdError
 from oauthlib.oauth2.rfc6749.errors import UnsupportedResponseTypeError
+from oauthlib.oauth2.rfc6749.errors import UnauthorizedClientError
 from sys import exit
+from base64 import b64decode
 
 def mysql_execute(command):
 	try:
@@ -29,28 +30,52 @@ class LocalboxRequestValidator(RequestValidator):
 	def validate_client_id(self, client_id, request):
 		# validates the client_id. Since this is either 'ios' or 'android', and this
 		# source code document is publicly available, the amount of security gained
-		#from this is practially none.
+		# from this is practially none.
 		result = mysql_execute("select * from clients where id = " + client_id)
 		return (len(result) == 1)
 
 	def get_default_redirect_uri(self, client_id, request):
 		return '/authenticated'
+
 	def validate_response_type(self, client_id, response_type, client, request, *args, **kwargs):
-		return True
+		types = response_type.split(" ")
+		return_value = True
+		for rtype in types:
+			if not rtype in ['code', 'token']:
+				return_value = False
+				print "falsified response type"
+		return return_value
+
 	def get_default_scopes(self, client_id, request, *args, **kwargs):
 		return ''
+
 	def validate_scopes(self, client_id, scopes, client, request, *args, **kwargs):
 		return True
 
+def HTTPAuthenticate(authorization_header_contents):
+	# example line: Basic bmlkbzpwYXNzOm9yZA==
+	# NOTE: this authentication implementation cannot handle usernames with colons.
+	authtype, authdata = authorization_header_contents.split(" ");
+	data = b64decode(authdata)
+	username, password = data.split(":",1)
+	#TODO: Not this
+	result = mysql_execute("select * from users where name = '" + username + "' and pass = '" + password + "';")
+	return(len(result) == 1)
+
 class OAuth2HTTPRequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
+		authenticationheader = self.headers.getheader('Authorization')
+		if authenticationheader != '':
+			if HTTPAuthenticate(authenticationheader):
+				print "Authentication successful. Now to intergrate it with the rest"
 		authserver = WebApplicationServer(LocalboxRequestValidator())
 		content_length = self.headers.getheader('Content-Length',0)
 		body = self.rfile.read(content_length)
 		print "GETTINGUH"
 		try:
 			result = authserver.validate_authorization_request(self.path, self.command, body, self.headers.dict)
-			self.send_response(200)
+			self.send_response(307)
+			self.send_header('Location', self.headers.getheader('redirect_uri', '/authenticated')) # TODO default value?
 			for key, value in result[1].iteritems():
 				self.send_header(key, value)
 			self.send_header("Content-type", "text/html")
@@ -71,6 +96,11 @@ class OAuth2HTTPRequestHandler(BaseHTTPRequestHandler):
 			self.send_error(400, "Sent response_type header invalid")
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
+		except UnauthorizedClientError as e:
+			self.send_error(400, "Client not recognised as valid, possibly response_type has been malformed")
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+
 		print "DONNU GET"
 		return
 
@@ -79,6 +109,6 @@ def run():
 	httpd = HTTPServer(server_address, OAuth2HTTPRequestHandler)
 	print('http server is running')
 	httpd.serve_forever()
-  
+
 if __name__ == '__main__':
 	run()
