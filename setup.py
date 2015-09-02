ca9e5fe9035cae346c4e631ac7a641f6e133a810
Nido Media <nido@localhost.localdomain>
2015-09-02 12:56:43 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "loauth",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Libbit",
+    author_email = "ivo@libbit.eu",
+    description = "standalone oauth2 authenticatiemodule",
+    license =  "EUPL",
+    url = "http://libbit.eu/loauth2",
+)
