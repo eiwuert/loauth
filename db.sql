73620bd8239cd0ed22b1f2f23d43418e024243af
wilson-santos-pf <wilson.santos@penguinformula.com>
2016-09-29 14:23:38 +0100

    db.sql is not necessary since database creating is using query in __init__

--- db.sql
+++
@@ -1,8 +0,0 @@
-create database 'test';
-create table users (user char(255), pass char(255));
-create table clients (id char(255), secret char(255), user char(255));
-create table authentication_code(client_id char(31), authcode char(31));
-create table bearer_tokens(access_token char(31), refresh_token char(31), expires datetime, scopes char(255), client_id char(255));
-
--- interesting queries:
--- delete outdated bearer tokens -- delete from bearer_tokens where expires < now();
